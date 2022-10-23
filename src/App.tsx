6590d2f25041418cdd28f06dec86cee1c35f74a5
Kamrad33 <comradebender33ru@gmail.com>
2022-10-24 00:12:40 +0300

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './styles/App.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className ='App'> 
+      <header>
+        <div className='header_name'>Манчкин онлайн</div>
+        <div className='header_buttons'>
+          <div className='btnHead'>Калькулятор боя</div>
+          <div className='btnHead'>Правила</div>
+          <div className='btnHead'>Войти</div>
+        </div>
       </header>
-    </div>
+    <div className='App_content'>
+      <div className='App_infoText'>
+        <div className='featuresTitle'>Эмулятор партии в манчкин теперь в твоём кармане!</div>
+          <div className='featuresText'>Добро пожаловать, манчкин! Здесь ты найдёшь весь необохимый функционал для комфортной партии в твою любимую игру:</div>
+            <ul className='featuresUl'>
+              <li><b>Калькулятор боя</b>, для удобного подсчёта баффов и дебаффов;</li>
+              <li><b>Список правил </b>по разделам для решения спорных ситуаций;</li>
+              <li><b>Эмулятор партии </b>на несколько человек с подсчётом всех карточек шмоток, проклятий и тд.;</li>
+              <li><b>Сохранение прогресса*</b> и статистика игр с друзьями для соревнования.</li>
+            </ul>
+          </div>
+      <div className='App_startContent'>
+        <div className='App_startContent_startGame_button'>Начать партию</div>
+        <div className='App_startContent_startGame_img'></div>
+        <div className='App_startContent_startGame_bottomText'>*Функционал сохранения сессий доступен только после регистрации</div>
+      </div>
+      </div>
+      <footer>
+        2022
+      </footer>
+      </div>
+   
   );
 }
 
